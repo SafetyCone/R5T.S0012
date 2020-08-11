83f0ee132da378acce9039cc22f25987e0f6c23e
David Coats <david@getsafetycone.com>
2020-08-10 23:49:03 -0700

    Added echoing functionality with exit. Initial commit of souce stub.

---
+++ source/R5T.S0012/Code/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace R5T.S0012
+{
+    class Program
+    {
+        const string ExitCommandValue = "exit";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n--- R5T.S0012 - Example echoing console program ---\n\n");
+            Console.WriteLine($"To exit type '{Program.ExitCommandValue}'.");
+
+            while(true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Type something:");
+
+                var line = Console.ReadLine();
+
+                var isExitCommand = Program.IsExitCommand(line);
+                if(isExitCommand)
+                {
+                    break;
+                }
+
+                Console.WriteLine(line);
+            }
+        }
+
+        private static bool IsExitCommand(string line)
+        {
+            var loweredLine = line.ToLowerInvariant();
+
+            var isExitCommand = loweredLine == Program.ExitCommandValue;
+            return isExitCommand;
+        }
+    }
+}
